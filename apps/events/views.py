1f360c647b57913894e3c8e74d8005af50597ab4
Saya Mirkulova <zansaaumirkulova@gmail.com>
2025-11-29 16:29:05 +0500

    updated by Saya

--- apps/events/views.py
+++ apps/events/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
-from rest_framework import generics
+# Python modules
+from typing import Any
+
+# Django Modules
+from django.db.models import QuerySet, Count
 from django.views.generic import ListView, DetailView
+
+# Django Rest Framework Modules
+from rest_framework import generics
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request as DRFRequest
+from rest_framework.response import Response as DRFResponse
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_204_NO_CONTENT, HTTP_201_CREATED, HTTP_404_NOT_FOUND
+
+
+# Project Modules
 from .models import Event
 from .serializers import EventSerializer
 
@@ -11,6 +25,7 @@ class EventListView(generics.ListCreateAPIView):
     """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
 
 class EventDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -37,3 +52,116 @@ class EventPageDetailView(DetailView):
     model = Event
     template_name = 'events/event_detail.html'
     context_object_name = 'event'
+
+
+class EventViewSet(ViewSet):
+    """
+    Creating Event ViewSet for handling Event-related endpoints
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def list(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating GET request"""
+
+        all_events:  QuerySet[Event] = Event.objects.annotate(
+            users_count=Count('users', distinct=True)
+        ).all()
+
+        serializer: EventSerializer = EventSerializer(
+            all_events,
+            many=True,
+        )
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_200_OK
+        )
+
+
+    def create(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating POST request"""
+
+        serializer: EventSerializer = EventSerializer(
+            data=request.data
+        )
+
+        if not serializer.is_valid():
+            return DRFResponse(
+                data=serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
+
+        serializer.save()
+
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_201_CREATED,
+        )
+
+
+    def partial_update(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating PATCH request"""
+
+        try:
+            event: Event = Event.objects.get(id=kwargs['pk'])
+        except Event.DoesNotExist:
+            return DRFResponse(
+                data={
+                    'pk': [f'Event with id={kwargs['pk']} do not exists.']
+                },
+                status=HTTP_404_NOT_FOUND
+            )
+
+        serializer: EventSerializer = EventSerializer(
+            data=request.data,
+            instance=event,
+            partial=True,
+        )
+
+        serializer.is_valid(raise_exception=True)
+
+        serializer.save()
+
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_200_OK,
+        )
+
+
+    def destroy(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating PATCH request"""
+
+        try:
+            event: Event = Event.objects.get(id=kwargs['pk'])
+        except Event.DoesNotExist:
+            return DRFResponse(
+                data={
+                    'pk': [f'Event with id={kwargs['pk']} do not exists.']
+                },
+                status=HTTP_404_NOT_FOUND
+            )
+
+        event.delete()
+
+        return DRFResponse(
+            status=HTTP_204_NO_CONTENT
+        )
--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
-from rest_framework import generics
+# Django Modules
 from django.views.generic import ListView, DetailView
+from django.db.models import QuerySet, Count
+
+# Django Rest Framework Modules
+from rest_framework import generics
+from rest_framework.viewsets import ViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.request import Request as DRFRequest
+from rest_framework.response import Response as DRFResponse
+from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST, HTTP_201_CREATED, HTTP_404_NOT_FOUND, HTTP_204_NO_CONTENT
+
+
+# Project Modules
 from .models import Post
 from .serializers import PostSerializer
+
+# Python Modules
+from typing import Any
 
 
 class PostListView(generics.ListCreateAPIView):
@@ -11,6 +25,7 @@ class PostListView(generics.ListCreateAPIView):
     """
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
 
 class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -37,3 +52,115 @@ class PostPageDetailView(DetailView):
     model = Post
     template_name = 'posts/post_detail.html'
     context_object_name = 'post'
+
+
+class PostViewSet(ViewSet):
+    """
+        Creating Post ViewSet for handling Event-related endpoints
+    """
+    permission_classes = (IsAuthenticated,)
+
+    def list(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            ** kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating GET request"""
+
+        all_posts: QuerySet[Post] = Post.objects.all()
+
+        serializer: PostSerializer = PostSerializer(
+            all_posts,
+            many=True,
+        )
+
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_200_OK
+        )
+
+
+    def create(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating POST request"""
+
+        serializer: PostSerializer = PostSerializer(
+            data=request.data
+        )
+
+        if not serializer.is_valid():
+            return DRFResponse(
+                data=serializer.errors,
+                status=HTTP_400_BAD_REQUEST,
+            )
+
+        serializer.save()
+
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_201_CREATED,
+        )
+
+
+    def partial_updateself(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating PATCH request"""
+
+        try:
+            post: Post = Post.objects.get(id=kwargs['id'])
+        except Post.DoesNotExist:
+            return DRFResponse(
+                data={
+                    'pk': [f'Post with that id={kwargs['pk']} does not exists']
+                },
+                status=HTTP_404_NOT_FOUND
+            )
+
+        serializer: PostSerializer = PostSerializer(
+            data=request.data,
+            instance=post,
+            partial=True,
+        )
+
+        serializer.is_valid()
+
+        serializer.save()
+
+        return DRFResponse(
+            data=serializer.data,
+            status=HTTP_200_OK,
+        )
+
+
+    def destroy(
+            self,
+            request: DRFRequest,
+            *args: tuple[Any, ...],
+            **kwargs: dict[str, Any],
+    ) -> DRFResponse:
+        """ Creating DELETE request"""
+
+        try:
+            post: Post = Post.objects.get(id=kwargs['id'])
+        except Post.DoesNotExist:
+            return DRFResponse(
+                data={
+                    'pk': [f'Post with that id={kwargs['pk']} does not exists']
+                },
+                status=HTTP_404_NOT_FOUND
+            )
+
+        post.delete()
+
+        return DRFResponse(
+            status=HTTP_204_NO_CONTENT
+        )
