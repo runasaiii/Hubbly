016fb21db54ef1138c25ae793da309d2fb3ceb35
runasaiii <arunakaragulova123@gmail.com>
2025-12-27 20:20:21 +0500

    Tests

--- apps/posts/tests/test_post_endpoints.py
+++ apps/posts/tests/test_post_endpoints.py
@@ -1,10 +1,11 @@
-# Python modules
+# DRF
 from rest_framework.test import APIClient
 from rest_framework import status
+
+# Python modules
 from uuid import uuid4
 
 # Django modules
-from django.urls import reverse
 from django.test import TestCase
 
 # Project modules
@@ -54,32 +55,27 @@ class PostEndpointsTests(TestCase):
         self.comments_url = f"/posts/{self.post.id}/comments/"
         self.like_url = f"/posts/{self.post.id}/like/"
 
-# List posts
     def test_list_posts_success(self):
         """Successfully get list of posts"""
         response = self.client.get(self.list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreaterEqual(len(response.data), 1)
 
-    def test_list_posts_without_auth(self):
-        """Get posts without authentication"""
-        self.client.force_authenticate(user=None)
-        response = self.client.get(self.list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_list_posts_wrong_method_put(self):
+        """Use wrong http method put"""
+        response = self.client.put(self.list_url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_list_posts_empty(self):
-        """No posts in database"""
-        Post.objects.all().delete()
-        response = self.client.get(self.list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+    def test_list_posts_wrong_method_delete(self):
+        """Use wrong http method delete"""
+        response = self.client.delete(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-    def test_list_posts_with_community_filter(self):
-        """Get posts filtered by community"""
-        response = self.client.get(self.list_url, {'community': str(self.community.id)})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_list_posts_wrong_method_patch(self):
+        """Use wrong http method patch"""
+        response = self.client.patch(self.list_url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-# Post creation 
     def test_create_post_success(self):
         """Successfully create post"""
         data = {'content': 'New post content'}
@@ -108,7 +104,6 @@ class PostEndpointsTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('content', response.data)
 
-# Post detail 
     def test_post_detail_success(self):
         """Successfully get post detail"""
         response = self.client.get(self.detail_url)
@@ -116,24 +111,22 @@ class PostEndpointsTests(TestCase):
         self.assertEqual(response.data['content'], "It's a test post")
 
     def test_post_detail_not_found(self):
-        """Get post detail with non-existent ID"""
+        """Get post detail with non-existent id"""
         invalid_url = f'/posts/api/{uuid4()}/'
         response = self.client.get(invalid_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_post_detail_invalid_id(self):
-        """Get post detail with invalid ID format"""
+        """Get post detail with invalid id format"""
         invalid_url = '/posts/api/invalid-id/'
         response = self.client.get(invalid_url)
-        self.assertIn(response.status_code, [status.HTTP_404_NOT_FOUND, status.HTTP_400_BAD_REQUEST])
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_post_detail_without_auth(self):
-        """Get post detail without authentication"""
-        self.client.force_authenticate(user=None)
-        response = self.client.get(self.detail_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_post_detail_wrong_method(self):
+        """Use wrong http method post on detail"""
+        response = self.client.post(self.detail_url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-# Post update 
     def test_update_post_success(self):
         """Partially update post successfully"""
         data = {'content': 'Updated post content'}
@@ -149,7 +142,7 @@ class PostEndpointsTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_update_post_invalid_id(self):
-        """Update post with non-existent ID"""
+        """Update post with non-existent id"""
         invalid_url = f'/posts/api/{uuid4()}/'
         data = {'content': 'Updated post'}
         response = self.client.patch(invalid_url, data, format='json')
@@ -159,15 +152,13 @@ class PostEndpointsTests(TestCase):
         """Update post with empty content"""
         data = {'content': ''}
         response = self.client.patch(self.detail_url, data, format='json')
-        # Empty content might be allowed for partial update
-        self.assertIn(response.status_code, [status.HTTP_200_OK, status.HTTP_400_BAD_REQUEST])
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-# Post delete 
     def test_delete_post_success(self):
         """Successfully delete post"""
         response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        self.assertFalse(Post.objects.filter(id=self.post.id, deleted_at__isnull=True).exists())
+        self.assertFalse(Post.objects.filter(id=self.post.id).exists())
 
     def test_delete_post_without_auth(self):
         """Delete post without authentication"""
@@ -176,7 +167,7 @@ class PostEndpointsTests(TestCase):
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_delete_post_invalid_id(self):
-        """Delete post with non-existent ID"""
+        """Delete post with non-existent id"""
         invalid_url = f'/posts/api/{uuid4()}/'
         response = self.client.delete(invalid_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
@@ -187,112 +178,104 @@ class PostEndpointsTests(TestCase):
         response = self.client.delete(self.detail_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-# ViewSet list 
     def test_v1_list_posts_success(self):
-        """Successfully get list of posts via ViewSet"""
+        """Successfully get list of posts via viewset"""
         response = self.client.get(self.v1_list_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertGreaterEqual(len(response.data), 1)
 
     def test_v1_list_posts_without_auth(self):
-        """Get posts via ViewSet without authentication"""
+        """Get posts via viewset without authentication"""
         self.client.force_authenticate(user=None)
         response = self.client.get(self.v1_list_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_v1_list_posts_empty(self):
-        """Get empty list via ViewSet"""
-        Post.objects.all().delete()
-        response = self.client.get(self.v1_list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
-
-    def test_v1_list_posts_wrong_method(self):
-        """Use wrong HTTP method"""
+    def test_v1_list_posts_wrong_method_put(self):
+        """Use wrong http method put via viewset"""
         response = self.client.put(self.v1_list_url, {}, format='json')
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-# ViewSet create 
+    def test_v1_list_posts_wrong_method_delete(self):
+        """Use wrong http method delete via viewset"""
+        response = self.client.delete(self.v1_list_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
     def test_v1_create_post_success(self):
-        """Successfully create post via ViewSet"""
+        """Successfully create post via viewset"""
         data = {'content': 'New post via ViewSet'}
         response = self.client.post(self.v1_list_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['content'], 'New post via ViewSet')
 
     def test_v1_create_post_without_auth(self):
-        """Create post via ViewSet without authentication"""
+        """Create post via viewset without authentication"""
         self.client.force_authenticate(user=None)
         data = {'content': 'New post'}
         response = self.client.post(self.v1_list_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_v1_create_post_empty_content(self):
-        """Create post via ViewSet with empty content"""
+        """Create post via viewset with empty content"""
         data = {'content': ''}
         response = self.client.post(self.v1_list_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_v1_create_post_missing_content(self):
-        """Create post via ViewSet without content"""
+        """Create post via viewset without content"""
         data = {}
         response = self.client.post(self.v1_list_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
- # ViewSet update 
     def test_v1_update_post_success(self):
-        """Successfully update post via ViewSet"""
+        """Successfully update post via viewset"""
         data = {'content': 'Updated via ViewSet'}
         response = self.client.patch(self.v1_detail_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['content'], 'Updated via ViewSet')
 
     def test_v1_update_post_without_auth(self):
-        """Update post via ViewSet without authentication"""
+        """Update post via viewset without authentication"""
         self.client.force_authenticate(user=None)
         data = {'content': 'Updated'}
         response = self.client.patch(self.v1_detail_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_v1_update_post_invalid_id(self):
-        """Update post via ViewSet with invalid ID"""
+        """Update post via viewset with invalid id"""
         invalid_url = f'/posts/v1/{uuid4()}/'
         data = {'content': 'Updated'}
         response = self.client.patch(invalid_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_v1_update_post_invalid_data(self):
-        """Update post via ViewSet with invalid data"""
-        data = {'invalid_field': 'value'}
+        """Update post via viewset with invalid data"""
+        data = {'content': ''}
         response = self.client.patch(self.v1_detail_url, data, format='json')
-        # Should still succeed for partial update
-        self.assertIn(response.status_code, [status.HTTP_200_OK, status.HTTP_400_BAD_REQUEST])
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
- # ViewSet delete 
     def test_v1_delete_post_success(self):
-        """Successfully delete post via ViewSet"""
+        """Successfully delete post via viewset"""
         response = self.client.delete(self.v1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
     def test_v1_delete_post_without_auth(self):
-        """Delete post via ViewSet without authentication"""
+        """Delete post via viewset without authentication"""
         self.client.force_authenticate(user=None)
         response = self.client.delete(self.v1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_v1_delete_post_invalid_id(self):
-        """Delete post via ViewSet with invalid ID"""
+        """Delete post via viewset with invalid id"""
         invalid_url = f'/posts/v1/{uuid4()}/'
         response = self.client.delete(invalid_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_v1_delete_post_twice(self):
-        """Delete post via ViewSet twice"""
+        """Delete post via viewset twice"""
         self.client.delete(self.v1_detail_url)
         response = self.client.delete(self.v1_detail_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-# User posts 
     def test_get_user_posts_success(self):
         """Successfully get posts by user"""
         url = f'/posts/v1/user/{self.user.id}/'
@@ -313,15 +296,12 @@ class PostEndpointsTests(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_get_user_posts_empty(self):
-        """Get posts for user with no posts"""
-        Post.objects.filter(author=self.other_user).delete()
-        url = f'/posts/v1/user/{self.other_user.id}/'
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+    def test_get_user_posts_wrong_method(self):
+        """Use wrong http method post on user posts"""
+        url = f'/posts/v1/user/{self.user.id}/'
+        response = self.client.post(url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
-# Comments 
     def test_list_comments_success(self):
         """Successfully get list of comments"""
         Comment.objects.create(
@@ -339,12 +319,15 @@ class PostEndpointsTests(TestCase):
         response = self.client.get(invalid_url)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_list_comments_empty(self):
-        """Get comments when no comments exist"""
-        Comment.objects.filter(post=self.post).delete()
-        response = self.client.get(self.comments_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 0)
+    def test_list_comments_wrong_method_put(self):
+        """Use wrong http method put on comments list"""
+        response = self.client.put(self.comments_url, {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_list_comments_wrong_method_delete(self):
+        """Use wrong http method delete on comments list"""
+        response = self.client.delete(self.comments_url)
+        self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_create_comment_success(self):
         """Successfully create comment"""
@@ -373,7 +356,6 @@ class PostEndpointsTests(TestCase):
         response = self.client.post(invalid_url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-# Like
     def test_like_post_success(self):
         """Successfully like a post"""
         response = self.client.post(self.like_url)
